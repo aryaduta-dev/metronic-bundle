aebd7ddda0fc04c74e2be1ea0ecca959c21fc57b
cibonfire <cibonfire@gmail.com>
2021-03-29 15:37:09 +0700

    v1

---
+++ src/DependencyInjection/MetronicExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Aryaduta\MetronicBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class MetronicExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container, new FileLocator(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+}
---
+++ src/MetronicBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Aryaduta\MetronicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MetronicBundle extends Bundle
+{
+
+}
